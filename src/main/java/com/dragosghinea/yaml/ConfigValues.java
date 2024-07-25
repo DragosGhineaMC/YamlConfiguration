3afda074a09a24b82469fe3b683ba80ac7bdb706
DragosGhinea <dragos.dev12@gmail.com>
2024-07-25 22:36:54 +0300

    Adding @SuperBuilder to ConfigValues for lombok further usage

--- src/main/java/com/dragosghinea/yaml/ConfigValues.java
+++ src/main/java/com/dragosghinea/yaml/ConfigValues.java
@@ -1,5 +1,10 @@
 package com.dragosghinea.yaml;
 
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@NoArgsConstructor
 public abstract class ConfigValues {
 
 }
